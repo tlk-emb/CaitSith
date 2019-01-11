0a77c61233a315bf6186a7186a420376c11340d1
takasehideki <takase@i.kyoto-u.ac.jp>
2019-01-11 18:11:26 +0900

    mix new

---
+++ lib/caitsith.ex
@@ -0,0 +1,18 @@
+defmodule Caitsith do
+  @moduledoc """
+  Documentation for Caitsith.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Caitsith.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/caitsith_test.exs
@@ -0,0 +1,8 @@
+defmodule CaitsithTest do
+  use ExUnit.Case
+  doctest Caitsith
+
+  test "greets the world" do
+    assert Caitsith.hello() == :world
+  end
+end
